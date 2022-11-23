69d1ca3dcda9e5e46117559ed5b5f71a868414bc
KluevJakov <kluevja@gmail.com>
2022-11-24 01:32:03 +0400

    init commit

---
+++ src/main/java/com/kluevja/bankappweb/BankappwebApplication.java
@@ -0,0 +1,13 @@
+package com.kluevja.bankappweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankappwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankappwebApplication.class, args);
+	}
+
+}
